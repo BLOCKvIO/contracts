cec9ced3893ae4ff7bdf3224ff4e6218ff885a89
ww <wojtanowski.w@gmail.com>
2017-11-03 14:46:42 +0100

    pool A - formula

--- erc20/contracts/PoolAContract.sol
+++ erc20/contracts/PoolAContract.sol
@@ -12,8 +12,14 @@ contract PoolAContract is Ownable {
 
     bool public done = false;
 
-    uint256 public conversionRate;
-    uint8 public tokenPrice;
+    uint multiplier = 10 ** 13;
+    uint decimals = 8; // to be discussed
+
+    // 69164622576285.7 * multiplier
+    uint256 public oneTokenInWei = 691646225762857000000000000;
+
+    uint defaultDiscount = 100;
+    mapping(uint8 => uint) discounts;
 
     address public ledgerContractAddr;
     address public blockVContractAddr;
@@ -21,26 +27,31 @@ contract PoolAContract is Ownable {
     BlockvToken blockVContract;
     BlockvPublicLedger ledgerContract; 
 
-    function PoolAContract(address _ledgerContractAddr, address _blockVContractAddr, uint _chunkSize, uint256 _conversionRate, uint8 _tokenPrice) {
+    function PoolAContract(address _ledgerContractAddr, address _blockVContractAddr, uint _chunkSize) {
         ledgerContractAddr = _ledgerContractAddr;
         blockVContractAddr = _blockVContractAddr;
 
         chunkSize = _chunkSize;
-        conversionRate = _conversionRate;
-        tokenPrice = _tokenPrice;
 
         blockVContract = BlockvToken(_blockVContractAddr);
         ledgerContract = BlockvPublicLedger(_ledgerContractAddr);
 
         ledgerContractSize = ledgerContract.distributionEntryCount();
+
+        // init discounts
+        // percent * multiplier
+        discounts[1] = 778486932785652;
+        discounts[2] = 80 * multiplier;
+        discounts[3] = 90 * multiplier;
+        discounts[100] = 100 * multiplier;
     }
 
-    function distribution() onlyOwner {
+    function distribution() public onlyOwner {
         require(!done);
 
         uint256 i = currentIndex;
         for (; i < currentIndex + chunkSize && i < ledgerContractSize; i++) {
-            var (, to, amount, discount, ) = ledgerContract.distributionList(i);
+            var (, to, amount, discount,) = ledgerContract.distributionList(i);
             uint256 tokenAmount = getTokenAmount(amount, discount);
             assert(blockVContract.transferFrom(msg.sender, to, tokenAmount));
         }
@@ -51,11 +62,21 @@ contract PoolAContract is Ownable {
         }
     }
 
-    function setChunkSize(uint _chunkSize) onlyOwner {
+    function setChunkSize(uint _chunkSize) public onlyOwner {
         chunkSize = _chunkSize;
     }
 
-    function getTokenAmount(uint256 amount, uint8 discount) private returns(uint256) {
-        return (amount * conversionRate * 100) / (tokenPrice * discount / 100);
+    function getTokenAmount(uint256 amount, uint8 discountGroup) private returns(uint256) {
+        uint discount = getTokenDiscount(discountGroup);
+        return amount * 10 ** decimals / (oneTokenInWei * discount) / 100 / multiplier;
+    }
+
+    function getTokenDiscount(uint8 discount) private returns(uint) {
+        uint r = discounts[discount];
+        if (r != 0) {
+            return r;
+        }
+        
+        return defaultDiscount * multiplier;
     }
 }
