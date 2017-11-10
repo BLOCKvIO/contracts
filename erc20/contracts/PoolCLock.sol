36e30147c9caac44359c5584fa22b1a10e77aad1
ww <wojtanowski.w@gmail.com>
2017-11-10 09:11:09 +0100

    payout cycles

--- erc20/contracts/PoolCLock.sol
+++ erc20/contracts/PoolCLock.sol
@@ -12,11 +12,17 @@ contract PoolCLock {
   // beneficiary of tokens after they are released
   address beneficiary;
 
+  uint startDay = now;
+
+  uint maxNumOfPayoutCycles = 5;
+
+  uint payoutCyclesLeft = 5;
+
   // timestamp where token release is enabled
-  uint releaseTime;
+  uint payoutCycleInDays = 180 days;
 
   // percent to be released every 6 months
-  uint8 constant percentOfTokens = 20;
+  uint8 constant percentToBeReleased = 20;
 
   // total amount of locked tokens
   uint256 totalAmount;
@@ -24,29 +30,40 @@ contract PoolCLock {
   // tokens to be released every 6 months
   uint256 tokensToBeReleased;
 
-  // lock time in days
-  uint constant releaseTimeDelay = 180 days;
-
   function PoolCLock(ERC20Basic _token, uint256 _totalAmount, address _beneficiary) {
     token = _token;
     beneficiary = _beneficiary;
-    releaseTime = now;
     totalAmount = _totalAmount;
 
-    tokensToBeReleased = SafeMath.div(SafeMath.mul(_totalAmount, percentOfTokens), 100);
+    tokensToBeReleased = SafeMath.div(SafeMath.mul(_totalAmount, percentToBeReleased), 100);
   }
 
   /**
    * @dev beneficiary claims tokens held by time lock
    */
   function claim() {
-    require(msg.sender == beneficiary);
-    require(now >= releaseTime);
+     require(msg.sender == beneficiary);
+     uint cycles = getPayoutCycles();
 
-    uint256 amount = token.balanceOf(this);
-    require(amount >= tokensToBeReleased);
+     require(cycles > 0);
 
-    releaseTime = now + releaseTimeDelay;
-    assert(token.transfer(beneficiary, tokensToBeReleased));
+     uint256 tbr = tokensToBeReleased * cycles;
+
+     uint256 amount = token.balanceOf(this);
+     require(amount >= tbr);
+
+     payoutCyclesLeft -= cycles;
+
+    assert(token.transfer(beneficiary, tbr));
+  }
+
+  function getPayoutCycles() private returns (uint) {
+      uint cycles = uint((now - startDay) / payoutCycleInDays) + 1;
+
+      if (cycles > maxNumOfPayoutCycles) {
+         cycles = maxNumOfPayoutCycles;
+      }
+
+      return cycles - (maxNumOfPayoutCycles - payoutCyclesLeft);
   }
 }
