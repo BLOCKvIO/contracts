899f2dc634cb369d0785d82e9bd3aec63037931b
ww <wojtanowski.w@gmail.com>
2017-11-19 15:11:11 +0100

    clean up pool c

--- erc20/contracts/PoolCLock.sol
+++ erc20/contracts/PoolCLock.sol
@@ -13,10 +13,10 @@ contract PoolCLock is PoolAllocations {
     maxNumOfPayoutCycles = 5; // 20% * 5 = 100%
     startDay = now;
     cyclesStartsFrom = 1; // the first payout cycles is released immediately
-    payoutCycleInDays = 2 minutes; //180 days; // 20% of tokens will be released every 6 months
+    payoutCycleInDays = 180 days; // 20% of tokens will be released every 6 months
 
     // allocations
-    allocations[0x11B48d3179Eb448bcf6b7340B146E98DC44474Da] = createAllocationEntry(182313562060012772996046012, 0, 36462712412002554599209202, 2);
+    allocations[0x905473A0d2f64087Fe4806e5c5911b35C7136290] = createAllocationEntry(182313562060012772996046012, 0, 36462712412002554599209202, 2);
     allocations[0x878688D7350bc396E54f092acd440F9eB99cce10] = createAllocationEntry(182313562060012772996046012, 0, 36462712412002554599209202, 2);
     allocations[0xb726bF40BEBf5fDde6916C75927235F9631227A7] = createAllocationEntry(182313562060012772996046012, 0, 36462712412002554599209202, 2);
     allocations[0x133914a31c0450e131211bB0558BD50465e8C39e] = createAllocationEntry(182313562060012772996046012, 0, 36462712412002554599209202, 2);
