913e88cad13726cdeac65dd0eda8e4fca52f68c5
ww <wojtanowski.w@gmail.com>
2017-11-20 17:00:02 +0100

    new values

--- erc20/contracts/PoolAContract.sol
+++ erc20/contracts/PoolAContract.sol
@@ -39,7 +39,7 @@ contract PoolAContract is Ownable {
 
         // init discounts
         // percent * discountMultiplier
-        discounts[1] = 78118333822467732419033555;
+        discounts[1] = 78975612990889553132979286;
         discounts[2] = 80 * discountMultiplier;
         discounts[3] = 90 * discountMultiplier;
         discounts[100] = 100 * discountMultiplier;
--- erc20/contracts/PoolDLock.sol
+++ erc20/contracts/PoolDLock.sol
@@ -5,7 +5,7 @@ import "./PoolAllocations.sol";
 
 contract PoolDLock is PoolAllocations {
 
-  uint256 public constant totalAmount = 546940686180038318988138036;
+  uint256 public constant totalAmount = 546940686180038280753511068;
 
   function PoolDLock(ERC20Basic _token) PoolAllocations(_token) {
     
@@ -17,22 +17,22 @@ contract PoolDLock is PoolAllocations {
 
     // allocations
     allocations[0x4311F6F65B411f546c7DD8841A344614297Dbf62] = createAllocationEntry(
-      182313562060012772996046012, // total
-      91156781030006386498023006, // first release
-      2532132806389066291611750,   // next release
-      6                           // the rest
+      182313562060012760251170356, // total
+      91156781030006380125585178,  // first release
+      2532132806389066114599588,   // next release
+      10                           // the rest
     );
      allocations[0x3b52Ab408cd499A1456af83AC095fCa23C014e0d] = createAllocationEntry(
-      182313562060012772996046012, // total
-      91156781030006386498023006, // first release
-      2532132806389066291611750,   // next release
-      6                           // the rest
+      182313562060012760251170356, // total
+      91156781030006380125585178,  // first release
+      2532132806389066114599588,   // next release
+      10                           // the rest
     );
      allocations[0x728D5312FbbdFBcC1b9582E619f6ceB6412B98E4] = createAllocationEntry(
-      182313562060012772996046012, // total
-      91156781030006386498023006, // first release
-      2532132806389066291611750,   // next release
-      6                           // the rest
+      182313562060012760251170356, // total
+      91156781030006380125585178,  // first release
+      2532132806389066114599588,   // next release
+      10                           // the rest
     );
   }
 }
